a6bc895d1574a85abd41287638d26b7a66c888fa
Boyan5 <b.milanov5@abv.bg>
2021-08-05 18:53:19 +0300

    First 3 problems in HackerRank algorithms section

---
+++ HackerRank/HackerRank/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 solveMeFirst
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+
+            //==============================================
+
+            //2 simpleArraySum
+            int[] ar = new int[] { 1, 1, 2, 3, 57, 1, 3, 4, 5 };
+            Console.WriteLine(simpleArraySum(ar));
+
+            //==============================================
+
+            //3 compareTriplets
+            List<int> a = new List<int>();
+            List<int> b = new List<int>();
+            a.Add(5);
+            a.Add(6);
+            a.Add(7);
+
+            b.Add(3);
+            b.Add(6);
+            b.Add(10);
+
+            foreach (var c in compareTriplets(a, b))
+                Console.WriteLine(c);
+        }
+
+        static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        //===================================================
+        static int simpleArraySum(int[] ar)
+        {
+            int sum = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
+            return sum;
+        }
+
+        //===================================================
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            var ScoreAlice = 0;
+            var ScoreBob = 0;
+            var i = 0;
+            for (i = 0; i < 3; i++)
+            {
+                if (a.ElementAt(i) > b.ElementAt(i))
+                {
+                    ScoreAlice++;
+                }
+                if (a.ElementAt(i) < b.ElementAt(i))
+                {
+                    ScoreBob++;
+                }
+            }
+            return new List<int> { ScoreAlice, ScoreBob };
+        }
+    }
+}
