10ba4af2bad480710b5483b47e8366dd96d0163e
Boyan5 <b.milanov5@abv.bg>
2021-08-09 22:53:25 +0300

    Number Line Jumps, Between Two Sets, Breaking the Records

--- HackerRank/HackerRank/Program.cs
+++ HackerRank/HackerRank/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp3
         {
             //1 solveMeFirst
             #region
+            Console.WriteLine("Solve me first");
             int val1 = Convert.ToInt32(Console.ReadLine());
             int val2 = Convert.ToInt32(Console.ReadLine());
             int sum = solveMeFirst(val1, val2);
@@ -19,12 +20,14 @@ namespace ConsoleApp3
 
             //2 simpleArraySum
             #region
+            Console.WriteLine("Simple Array Sum");
             int[] ar = new int[] { 1, 1, 2, 3, 57, 1, 3, 4, 5 };
             Console.WriteLine(simpleArraySum(ar));
             #endregion
 
             //3 compareTriplets
             #region
+            Console.WriteLine("Compare Triplets");
             List<int> a = new List<int>();
             List<int> b = new List<int>();
             a.Add(5);
@@ -41,12 +44,14 @@ namespace ConsoleApp3
 
             //4 aVeryBigSum
             #region
+            Console.WriteLine("A Very Big Sum");
             long[] arL = new long[] { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
             Console.WriteLine(aVeryBigSum(arL));
             #endregion
 
             //5 diagonalDifference
             #region
+            Console.WriteLine("Diagonal Difference");
             List<List<int>> list = new List<List<int>>();
             List<int> list1 = new List<int>();
             list1.Add(11);
@@ -70,35 +75,41 @@ namespace ConsoleApp3
 
             //6 plusMinus
             #region
+            Console.WriteLine("Plus Minus");
             int[] arrPlusMinus = new int[] { -4, 3, -9, 0, 4, 1 };
             plusMinus(arrPlusMinus);
             #endregion
 
             //7 staircase
             #region
+            Console.WriteLine("Staircase");
             staircase(6);
             #endregion
 
             //8 miniMaxSum
             #region
+            Console.WriteLine("MiniMax Sum");
             int[] arrMiniMaxSum = new int[] { 1, 2, 3, 4, 5 };
             miniMaxSum(arrMiniMaxSum);
             #endregion
 
             //9 birthdayCakeCandles 
             #region
+            Console.WriteLine("Birthday Cake Candles");
             int[] arrBirthdayCakeCandles = new int[] { 3, 2, 1, 3 };
             Console.WriteLine(birthdayCakeCandles(arrBirthdayCakeCandles));
             #endregion
 
-            //10 timeConvresion
+            //10 timeConversion
             #region
+            Console.WriteLine("Time Conversion");
             string stringtimeConversion = "07:05:45PM";
             Console.WriteLine(timeConversion(stringtimeConversion));
             #endregion
 
             //11 gradingStudents
             #region
+            Console.WriteLine("Grading Students");
             List<int> listgradingStudents = new List<int>();
             listgradingStudents.Add(73);
             listgradingStudents.Add(67);
@@ -111,6 +122,7 @@ namespace ConsoleApp3
 
             //12 countApplesAndOranges
             #region
+            Console.WriteLine("Count Apples and Oranges");
             int sAaO = 7;
             int tAaO = 11;
             int aAaO = 5;
@@ -119,6 +131,40 @@ namespace ConsoleApp3
             int[] oranges = new int[] { 5, -6 };
 
             countApplesAndOranges(sAaO, tAaO, aAaO, bAaO, apples, oranges);
+            #endregion
+
+            //13 kangoroo
+            #region
+            Console.WriteLine("Kangoroo");
+            int x1Kangoroo = 0;
+            int v1kangoroo = 3;
+            int x2Kangoroo = 4;
+            int v2Kangoroo = 2;
+
+            Console.WriteLine(kangaroo(x1Kangoroo, v1kangoroo, x2Kangoroo, v2Kangoroo));
+
+            #endregion
+
+            //14 getTotalX
+            #region
+            Console.WriteLine("Get Total X");
+            List<int> agetTotalX = new List<int>();
+            agetTotalX.Add(2);
+            agetTotalX.Add(4);
+            List<int> bgetTotalX = new List<int>();
+            bgetTotalX.Add(16);
+            bgetTotalX.Add(32);
+            bgetTotalX.Add(96);
+            Console.WriteLine(getTotalX(agetTotalX, bgetTotalX));
+            #endregion
+
+            //15 breakingRecords
+            #region
+            Console.WriteLine("Breaking Records");
+            int[] scoresBreakingRecords = new int[] { 10, 5, 20, 20, 4, 5, 2, 25, 1 };
+
+            foreach(var resultBreakingRecords in breakingRecords(scoresBreakingRecords))
+                Console.Write(resultBreakingRecords + " ");
             #endregion
 
         }
@@ -367,6 +413,85 @@ namespace ConsoleApp3
             Console.WriteLine(applesCounter);
             Console.WriteLine(orangesCounter);
 
+        }
+
+        static string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            int jumpsFirstKangaroo = 0;
+            int jumpsSecondKangaroo = 0;
+            int distanceKangarooOne = x1 + v1;
+            int distanceKangarooTwo = x2 + v2;
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (distanceKangarooOne == distanceKangarooTwo && jumpsFirstKangaroo == jumpsSecondKangaroo)
+                    return "YES";
+                else
+                {
+                    distanceKangarooOne += v1;
+                    distanceKangarooTwo += v2;
+                    jumpsFirstKangaroo++;
+                    jumpsSecondKangaroo++;
+                }
+            }
+            return "NO";
+
+
+        }
+
+        public static int getTotalX(List<int> a, List<int> b)
+        {
+            int foundCount = 0, maxA = a.Max(), minB = b.Min(), current = maxA;
+            while (current <= minB)
+            {
+                if (a.All(e => current % e == 0 || e % current == 0) && b.All(e => current % e == 0 || e % current == 0))
+                    foundCount++;
+                current += maxA;
+            };
+            return foundCount;
+
+        }
+
+        static int[] breakingRecords(int[] scores)
+        {
+            int lowestScore = scores[0];
+            int highestScore = scores[0];
+            bool brokeLowest = false;
+            bool brokeHighest = false;
+            int counterLowest = 0;
+            int counterHighest = 0;
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] < lowestScore)
+                {
+                    lowestScore = scores[i];
+                    brokeLowest = true;
+                }
+                if (brokeLowest)
+                {
+                    counterLowest++;
+                    brokeLowest = false;
+                }
+
+            }
+
+            for (int i = 01; i < scores.Length; i++)
+            {
+                if (scores[i] > highestScore)
+                {
+                    highestScore = scores[i];
+                    brokeHighest = true;
+                }
+                if (brokeHighest)
+                {
+                    counterHighest++;
+                    brokeHighest = false;
+                }
+
+            }
+
+            return new int[] { counterHighest, counterLowest };
         }
     }
 }
